ad4ec7b2412409f18fbfa45f60e92e996bee8c07
Nadia Bacchus <nadiabacchus@Nadias-MacBook-Pro.local>
2022-11-13 21:13:55 -0500

    commit

--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,6 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-const { dropAllSchemas } = require("../config/connection");
+//const { dropAllSchemas } = require("../config/connection");
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
